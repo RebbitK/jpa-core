60e1903d21ab6c6562ce15c823c28ec9cdff1cf0
RebbitK <andxl9979@gmail.com>
2024-01-22 17:25:18 +0900

    영속성 컨텍스트 학습

--- src/main/java/com/sparta/entity/Memo.java
+++ src/main/java/com/sparta/entity/Memo.java
@@ -8,7 +8,6 @@ import jakarta.persistence.*;
 @Table(name = "memo")
 public class Memo {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(name = "username", nullable = false, unique = true)
@@ -17,4 +16,28 @@ public class Memo {
 
     @Column(name = "contents", nullable = false, length = 500)
     private String contents;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getContents() {
+        return contents;
+    }
+
+    public void setContents(String contents) {
+        this.contents = contents;
+    }
 }
